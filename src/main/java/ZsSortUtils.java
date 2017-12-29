3fc2c98afc4db2280ba079c912caffe42bce2d7c
zeus <694814357@qq.com>
2017-12-29 15:00:21 +0800

    add

---
+++ src/main/java/ZsSortUtils.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+/**
+ * @author zeus
+ * @date 2017/11/20
+ */
+public class ZsSortUtils {
+
+    /**
+     * 以Map的value排序，并返回x
+     *
+     * @param oldMap
+     * @return
+     */
+    public static Map<Integer, Integer> sortMap(Map<Integer, Integer> oldMap) {
+
+        if (oldMap != null && !oldMap.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(oldMap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            }
+        });
+        Map<Integer, Integer> newMap = new LinkedHashMap<Integer, Integer>();
+        for (Iterator<Map.Entry<Integer, Integer>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<Integer, Integer> entry = it.next();
+            newMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return newMap;
+
+    }
+
+
+}
